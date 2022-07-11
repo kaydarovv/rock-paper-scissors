1c7f0bc3a1aec61de72501630c8374e79c7ca16a
kaydarovv <meavertex@gmail.com>
2022-07-11 14:04:00 +0600

    add function to let comp and player play a round

--- main.js
+++ main.js
@@ -11,4 +11,29 @@ function computerPlay() {
   }
 }
 
-console.log(computerPlay());
+// function PlayRound compares computer's and player's inputs and decides the winner
+
+function playRound(playerChoice, computerChoice) {
+  playerChoice.toLowerCase();
+  computerChoice.toLowerCase();
+
+  if (playerChoice == "rock" && computerChoice == "scissors") {
+    return "You won! Rock beats Scissors";
+  } else if (playerChoice == "scissors" && computerChoice == "paper") {
+    return "You won! Scissors beats Paper";
+  } else if (playerChoice == "paper" && computerChoice == "rock") {
+    return "You won! Paper beats Rock";
+  } else if (playerChoice == "scissors" && computerChoice == "rock") {
+    return "You lost! Rock beats Scissors";
+  } else if (playerChoice == "paper" && computerChoice == "scissors") {
+    return "You lost! Scissors beats Paper";
+  } else if (playerChoice == "rock" && computerChoice == "paper") {
+    return "You lost! Paper beats Rock";
+  }
+}
+
+playerChoice = prompt("Rock-Paper-Scissors... Which one do you choose?");
+
+computerChoice = computerPlay();
+
+playRound(playerChoice, computerChoice);
