71f30b8550a40670e107a0fcea4290e8232ee17a
kaydarovv <meavertex@gmail.com>
2022-07-11 14:40:46 +0600

    finish the game loop

--- main.js
+++ main.js
@@ -3,37 +3,77 @@
 function computerPlay() {
   let randNum = Math.ceil(Math.random() * 3);
   if (randNum == 1) {
-    return "Scissors";
+    return "scissors";
   } else if (randNum == 2) {
-    return "Rock";
+    return "rock";
   } else {
-    return "Paper";
+    return "paper";
   }
 }
 
-// function PlayRound compares computer's and player's inputs and decides the winner
+// function playRound() compares computer's and player's inputs and decides the winner
 
 function playRound(playerChoice, computerChoice) {
   playerChoice.toLowerCase();
   computerChoice.toLowerCase();
+  const roundResult = [];
 
   if (playerChoice == "rock" && computerChoice == "scissors") {
-    return "You won! Rock beats Scissors";
+    roundResult[0] = "You won! Rock beats Scissors";
+    roundResult[1] = 2;
   } else if (playerChoice == "scissors" && computerChoice == "paper") {
-    return "You won! Scissors beats Paper";
+    roundResult[0] = "You won! Scissors beats Paper";
+    roundResult[1] = 2;
   } else if (playerChoice == "paper" && computerChoice == "rock") {
-    return "You won! Paper beats Rock";
+    roundResult[0] = "You won! Paper beats Rock";
+    roundResult[1] = 2;
   } else if (playerChoice == "scissors" && computerChoice == "rock") {
-    return "You lost! Rock beats Scissors";
+    roundResult[0] = "You lost! Rock beats Scissors";
+    roundResult[1] = 1;
   } else if (playerChoice == "paper" && computerChoice == "scissors") {
-    return "You lost! Scissors beats Paper";
+    roundResult[0] = "You lost! Scissors beats Paper";
+    roundResult[1] = 1;
   } else if (playerChoice == "rock" && computerChoice == "paper") {
-    return "You lost! Paper beats Rock";
+    roundResult[0] = "You lost! Paper beats Rock";
+    roundResult[1] = 1;
+  } else {
+    roundResult[0] = "It's draw, replay the round!";
+    roundResult[1] = 0;
+  }
+  return roundResult;
+}
+
+// function game() allows to play the game 5 times with keeping a score
+
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+  let roundResult = [];
+  for (let i = 0; i < 5; i++) {
+    let playerChoice = prompt(
+      "Rock-Paper-Scissors... Which one do you choose?"
+    );
+    let computerChoice = computerPlay();
+
+    roundResult = playRound(playerChoice, computerChoice);
+
+    if (roundResult[1] == 2) {
+      playerScore++;
+    } else if (roundResult[1] == 1) {
+      computerScore++;
+    }
+
+    console.log(
+      `${roundResult} \n Player: ${playerScore} \n Computer: ${computerScore}`
+    );
+  }
+  if (playerScore > computerScore) {
+    console.log("Player won!");
+  } else if (playerScore < computerScore) {
+    console.log("Computer won!");
+  } else {
+    console.log("A draw!");
   }
 }
 
-playerChoice = prompt("Rock-Paper-Scissors... Which one do you choose?");
-
-computerChoice = computerPlay();
-
-playRound(playerChoice, computerChoice);
+game();
